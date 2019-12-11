fafcb644d8d75a69b10768353b26eb4c9ca4bd98
underTheTitle <underthetitle@gmail.com>
2019-12-12 00:30:10 +0900

    data object까지

---
+++ vueClass/Fundamentals/product.js
@@ -0,0 +1,17 @@
+const items = [
+    {
+        name: '연필',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: '공책',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: '지우개',
+        price: 500,
+        quantity: 0
+    }
+]
